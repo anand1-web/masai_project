3144b8760be5374e9ff09315a55d746746bcbd6b
conal-exploits <madhavk6495@gmail.com>
2022-09-13 21:12:53 +0530

    login logic

--- login.js
+++ login.js
@@ -1,2 +1,12 @@
 let reg_email="abc@gmail.com";
 let reg_pass="123456";
+
+let enter_email="abc@gmail.com";
+
+let enter_pass="123456";
+
+if(reg_email === enter_email && reg_pass === enter_pass){
+console.log("Login succesfully");
+}
+else{
+console.log("Login failed");
